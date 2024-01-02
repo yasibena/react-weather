b00a561b46284b5452e5c5bb3ceb08de29c5b0c9
yasibena <118657398+yasibena@users.noreply.github.com>
2024-01-02 17:16:39 +0330

    upload project on github

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css"; // Import your styles
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
